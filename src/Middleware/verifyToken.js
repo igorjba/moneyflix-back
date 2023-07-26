eadfa4d6857953b3fc3bf057c2f06c0764d4b23d
Igor Bahia <115411211+igorjba@users.noreply.github.com>
2023-07-26 16:24:16 +0000

    :sparkles: feat: arquivo de verificação do token modificado.

--- src/Middleware/verifyToken.js
+++ src/Middleware/verifyToken.js
@@ -2,21 +2,18 @@ const jwt = require("jsonwebtoken");
 const passJWT = process.env.passJWT;
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).json({ error: 'Token não fornecido' });
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(400).json({ message: "Não autorizado" });
   }
+  try {
+    const token = authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, passJWT);
+
     next();
   } catch (error) {
     return res.status(401).json(error.message);
-
   }
-  jwt.verify(token, passJWT, (err, decoded) => {
-    if (err) return res.status(401).json({ error: 'Token inválido' });
-
-    req.userId = decoded.id;
-    return next();
-  });
 };
 
 module.exports = { verifyToken };
