cec5c7d8822455e04f2e9c7b26d5addc354845f1
Igor <igorjbahia@gmai.com>
2023-07-26 12:54:02 -0300

    :sparkles: feat: aplicando ajustes na verificação do token.

--- src/Middleware/verifyToken.js
+++ src/Middleware/verifyToken.js
@@ -7,11 +7,13 @@ const verifyToken = (req, res, next) => {
     return res.status(400).json({ message: "Não autorizado" });
   }
   try {
-    const token = authorization.split(" ")[1];
+    const token = authorization.replace("Bearer ", "");
     const decodedToken = jwt.verify(token, passJWT);
+    console.log(decodedToken); // log the decoded token
 
     next();
   } catch (error) {
+    console.log(error); // log any errors that occur
     return res.status(401).json({ message: "Token inválido" });
   }
 };
