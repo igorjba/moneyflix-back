3de980b9c4703c805e780ff699c135f7b72e6096
Igor <igorjbahia@gmai.com>
2023-07-26 13:57:45 -0300

    :sparkles: feat: modificado o tratamento do erro no middleware verifyToken.

--- src/Middleware/verifyToken.js
+++ src/Middleware/verifyToken.js
@@ -4,14 +4,14 @@ const passJWT = process.env.passJWT;
 const verifyToken = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    return res.status(400).json({ message: "Não autorizado" });
+    return res.status(500).json({ message: "Token não encontrado" });
   }
   try {
     const token = authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, passJWT);
     next();
   } catch (error) {
-    return res.status(401).json(error.message);
+    return res.status(500).json({ message: "Não autorizado" });
   }
 };
 
